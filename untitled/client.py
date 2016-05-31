5825a4d5f6fcc9e9a477fd185e924a611289b6a9
Gribun <gt515ru@gmail.com>
2016-05-31 20:04:39 +0600

    asd

--- untitled/client.py
+++ untitled/client.py
@@ -5,6 +5,8 @@ sock = socket.socket()
 sock.connect(("192.168.0.9", 8080))
 while True:
     inp = input("Enter msg: ")
+    if inp == '':
+        inp =' '
     sock.send(inp.encode("utf-8"))
     request = sock.recv(1024)
     print(request.decode("utf-8"))
