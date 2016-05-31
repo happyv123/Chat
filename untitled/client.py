9d9d3852c1882c5611e3ea28844440afd8ac0a7e
Gribun <gt515ru@gmail.com>
2016-05-31 20:27:48 +0600

    C

--- untitled/client.py
+++ untitled/client.py
@@ -2,7 +2,7 @@ import socket
 
 
 sock = socket.socket()
-sock.connect(("192.168.0.9", 8080))
+sock.connect(("192.168.0.9", 8081))
 while True:
     inp = input("Enter msg: ")
     if inp == '':
--- untitled/server.py
+++ untitled/server.py
@@ -6,7 +6,7 @@ import sys
 sock = socket.socket()
 sock.bind(("", 8080))
 sock.listen(10)
-
+req=[]
 while True:
     client_socket, remote_address = sock.accept()
     child_pid = os.fork()
@@ -14,7 +14,11 @@ while True:
         while True:
             request = client_socket.recv(1024)
             client_socket.send(request.upper())
-            print('{} : {}'.format(client_socket.getpeername(), request))
+            request_str = request.decode("utf-8")
+            req.append('{} : {}'.format(client_socket.getpeername(), request_str))
+            for i in range(0, len(req)):
+                print(req[i])
+            print('{} : {}'.format(client_socket.getpeername(), request_str))
         sys.exit()
     else:
         pass
