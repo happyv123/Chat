31b5d647e95ed492502fb3a031932e8fd2f5749e
Gribun <gt515ru@gmail.com>
2016-05-31 21:01:29 +0600

    S

--- untitled/client.py
+++ untitled/client.py
@@ -1,12 +1,12 @@
 import socket
 
-
-sock = socket.socket()
-sock.connect(("192.168.0.9", 8081))
 while True:
     inp = input("Enter msg: ")
+    sock = socket.socket()
+    sock.connect(("192.168.0.9", 8080))
     if inp == '':
         inp =' '
     sock.send(inp.encode("utf-8"))
     request = sock.recv(1024)
     print(request.decode("utf-8"))
+    sock.close()
--- untitled/server.py
+++ untitled/server.py
@@ -3,24 +3,24 @@ import socket
 import sys
 
 
+o = open("file.txt", 'a')
 sock = socket.socket()
 sock.bind(("", 8080))
 sock.listen(10)
-req=[]
-while True:
-    client_socket, remote_address = sock.accept()
-    child_pid = os.fork()
-    if child_pid == 0:
-        while True:
+req = []
+try:
+    while True:
+        client_socket, remote_address = sock.accept()
+        child_pid = os.fork()
+        if child_pid == 0:
             request = client_socket.recv(1024)
             client_socket.send(request.upper())
-            request_str = request.decode("utf-8")
-            req.append('{} : {}'.format(client_socket.getpeername(), request_str))
-            for i in range(0, len(req)):
-                print(req[i])
-            print('{} : {}'.format(client_socket.getpeername(), request_str))
-        sys.exit()
-    else:
-        pass
-
-sock.close()
+            print('{} : {}'.format(client_socket.getpeername(), request))
+            o.write('{} : {}'.format(client_socket.getpeername(), request) + '\n')
+            client_socket.close()
+            sys.exit()
+        else:
+            client_socket.close()
+except KeyboardInterrupt:
+    sock.close()
+    o.close()
