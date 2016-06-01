ff2a62cdd8df30e920dbf7ca4a466fc01008c677
Gribun <gt515ru@gmail.com>
2016-06-01 14:20:17 +0600

    Hu

--- untitled/client.py
+++ untitled/client.py
@@ -1,15 +1,13 @@
 import socket
 sock = socket.socket()
 sock.connect(("192.168.0.9", 8080))
-sock.send()
+inp = input("Entry login: ")
+sock.send(inp.encode('utf-8'))
 
-try:
-    while True:
-        inp = input("Enter msg: ")
-        if inp == '':
-            inp =' '
-        sock.send(inp.encode("utf-8"))
-        request = sock.recv(1024)
-        print(request.decode("utf-8"))
-except KeyboardInterrupt:
-    sys.exit()
+while True:
+    inp = input("Enter msg: ")
+    if inp == '':
+        inp =' '
+    sock.send(inp.encode("utf-8"))
+    request = sock.recv(1024)
+    print(request.decode("utf-8"))
