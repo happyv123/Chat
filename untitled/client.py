2304c0ce9097e3cfaadbf9507ef7482ea160b88d
Gribun <gt515ru@gmail.com>
2016-06-01 14:16:49 +0600

    S

--- untitled/client.py
+++ untitled/client.py
@@ -1,13 +1,15 @@
 import socket
 sock = socket.socket()
 sock.connect(("192.168.0.9", 8080))
+sock.send()
 
-
-while True:
-    inp = input("Enter msg: ")
-    if inp == '':
-        inp =' '
-    sock.send(inp.encode("utf-8"))
-    request = sock.recv(1024)
-    print(request.decode("utf-8"))
-sock.close()
+try:
+    while True:
+        inp = input("Enter msg: ")
+        if inp == '':
+            inp =' '
+        sock.send(inp.encode("utf-8"))
+        request = sock.recv(1024)
+        print(request.decode("utf-8"))
+except KeyboardInterrupt:
+    sys.exit()
--- untitled/server.py
+++ untitled/server.py
@@ -2,8 +2,7 @@ import os
 import socket
 import sys
 
-
-o = open("file.txt", 'w')
+o = open("file.txt", 'a')
 sock = socket.socket()
 sock.bind(("", 8080))
 sock.listen(10)
@@ -12,19 +11,28 @@ tcpre = {}
 try:
     while True:
         client_socket, remote_address = sock.accept()
+        login = client_socket.recv(1024).decode('utf-8')
         address = remote_address[0]
+        tcpre = {address: login}
         child_pid = os.fork()
         if child_pid == 0:
             while True:
                 request = client_socket.recv(1024)
+                if request.decode('utf-8') == '':
+                    client_socket.close()
+                    print('User {} has left'.format(tcpre[address]))
+                    sys.exit()
                 client_socket.send(request.upper())
                 request_str = request.decode("utf-8")
-                print('{} : {}'.format(address, request_str))
-                o.write('{} : {}'.format(address, request_str) + '\n')
-            client_socket.close()
-            sys.exit()
+                print('{} : {}'.format(tcpre[address], request_str))
+                o.write('{} : {}'.format(tcpre[address], request_str) + '\n')
+
         else:
             client_socket.close()
 except KeyboardInterrupt:
+    sock.close()
+    o.close()
+except ConnectionResetError:
+    print('User {} has left'.format(tcpre[address]))
     sock.close()
     o.close()
