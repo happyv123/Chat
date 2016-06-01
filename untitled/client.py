94e4fc37edd66d3772b66e731f24792ff58aea2a
Gribun <gt515ru@gmail.com>
2016-06-01 13:50:00 +0600

    S

--- untitled/client.py
+++ untitled/client.py
@@ -1,12 +1,13 @@
 import socket
+sock = socket.socket()
+sock.connect(("192.168.0.9", 8080))
+
 
 while True:
-    sock = socket.socket()
     inp = input("Enter msg: ")
-    sock.connect(("192.168.0.9", 8080))
     if inp == '':
         inp =' '
     sock.send(inp.encode("utf-8"))
     request = sock.recv(1024)
     print(request.decode("utf-8"))
-    sock.close()
+sock.close()
--- untitled/server.py
+++ untitled/server.py
@@ -8,16 +8,19 @@ sock = socket.socket()
 sock.bind(("", 8080))
 sock.listen(10)
 req = []
+tcpre = {}
 try:
     while True:
         client_socket, remote_address = sock.accept()
+        address = remote_address[0]
         child_pid = os.fork()
         if child_pid == 0:
-            request = client_socket.recv(1024)
-            client_socket.send(request.upper())
-            request_str = request.decode("utf-8")
-            print('{} : {}'.format(client_socket.getpeername(), request_str))
-            o.write('{} : {}'.format(client_socket.getpeername(), request_str) + '\n')
+            while True:
+                request = client_socket.recv(1024)
+                client_socket.send(request.upper())
+                request_str = request.decode("utf-8")
+                print('{} : {}'.format(address, request_str))
+                o.write('{} : {}'.format(address, request_str) + '\n')
             client_socket.close()
             sys.exit()
         else:
